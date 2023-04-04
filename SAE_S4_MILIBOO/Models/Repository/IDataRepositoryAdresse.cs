ceb46b708d96db1658be3f9dbbc08b9645136718
Aymeric-a <aymericphyt@gmail.com>
2023-04-04 10:30:15 +0200

    gyidghk

--- SAE_S4_MILIBOO/Models/Repository/IDataRepositoryAdresse.cs
+++ SAE_S4_MILIBOO/Models/Repository/IDataRepositoryAdresse.cs
@@ -5,7 +5,6 @@ namespace SAE_S4_MILIBOO.Models.Repository
     public interface IDataRepositoryAdresse<TEntity>
     {
         Task<ActionResult<TEntity>> GetByIdAsync(int id);
-        Task<ActionResult<IEnumerable<TEntity>>> GetAllByVilleAsync(string ville);
         Task AddAsync(TEntity entity);
         Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
         Task DeleteAsync(TEntity entity);
